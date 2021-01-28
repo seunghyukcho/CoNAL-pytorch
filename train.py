038bcad67c4e302214aeb21e17d60c7fc00fd7f0
seunghyukcho <shhj1998@postech.ac.kr>
2021-01-28 22:49:49 +0900

    finished implementing prototype of training CoNAL

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+from torch.optim import Adam
 from torch.utils.data import DataLoader
 
 from dataset import LabelMeDataset
@@ -9,12 +11,22 @@ from classifier import LabelMeClassifier
 if __name__ == "__main__":
     train_dataset = LabelMeDataset('data/LabelMe/train', 'data/LabelMe/images', True)
     classifier = LabelMeClassifier()
-    model = CoNAL(3 * 256 * 256, 8, train_dataset.annotations.shape[1], classifier).double()
+    model = CoNAL(3 * 256 * 256, 8, train_dataset.annotations.shape[1], classifier).cuda()
+    criterion = nn.CrossEntropyLoss(ignore_index=-1)
+    optimizer = Adam(model.parameters(), lr=1e-3)
 
     train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
-    for epoch in range(1):
+    for epoch in range(10):
         for x, y, annotation in train_loader:
-            annotator = torch.eye(77)
+            model.zero_grad()
+            x, y, annotation = x.cuda(), y.cuda(), annotation.cuda()
+            annotator = torch.eye(77).cuda()
             result = model(x, annotator)
 
-            print(result.shape)
+            result = torch.reshape(result, (-1, 8))
+            annotation = annotation.view(-1)
+
+            loss = criterion(result, annotation)
+            loss.backward()
+            optimizer.step()
+            print(loss.item())
